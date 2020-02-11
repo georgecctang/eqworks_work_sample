0729ac9e749e7b84e90afa8d182bab558f154893
georgecctang <48698540+georgecctang@users.noreply.github.com>
2020-02-11 15:38:23 -0500

    Add files via upload

---
+++ submission/function/haversine_function.py
@@ -0,0 +1,13 @@
+# Define a basic Haversine distance formula
+# reference:
+import numpy as np
+
+def haversine(lat1, lon1, lat2, lon2):
+    KM = 6372.8
+    lat1, lon1, lat2, lon2 = map(np.deg2rad, [lat1, lon1, lat2, lon2])
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
+    c = 2 * np.arcsin(np.sqrt(a))
+    total_km = KM * c
+    return total_km
